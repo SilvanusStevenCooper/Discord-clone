48004ca6ab7d9de6a88b066008d526ee1468e611
steve <coopersilvanus27@gmail.com>
2024-11-28 17:24:56 +0000

    issue 6 fixed. App up to date

--- components/modles/edit-server-model.tsx
+++ components/modles/edit-server-model.tsx
@@ -66,7 +66,7 @@ const EditServerModel = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      await axios.patch(`/api/servers/${server.id}`, values);
+      await axios.patch(`/api/servers/${server?.id}`, values);
 
       form.reset();
       router.refresh();
