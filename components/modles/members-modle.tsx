5826b79a85f0eb77883d048b6350a6e2032f2a7d
steve <coopersilvanus27@gmail.com>
2024-11-28 16:47:24 +0000

    issue 4 fixed. App up to date

--- components/modles/members-modle.tsx
+++ components/modles/members-modle.tsx
@@ -112,7 +112,7 @@ const MembersModel = () => {
           </DialogDescription>
         </DialogHeader>
         <ScrollArea className="mt-8 mx-h-[420px] pr-6">
-          {server?.members?.map((member: Member) => {
+          {server?.members?.map((member: any) => {
             let roleIcon: any;
             switch (member.role) {
               case "ADMIN":
@@ -132,19 +132,19 @@ const MembersModel = () => {
             }
 
             return (
-              <div key={member.id} className="flex items-center gap-x-2 mb-6">
-                <UserAvatar src={member.profile.imageUrl} />
+              <div key={member?.id} className="flex items-center gap-x-2 mb-6">
+                <UserAvatar src={member?.profile.imageUrl} />
                 <div className="flex flex-col gap-y-1">
                   <div className="text-xs font-semibold flex items-center gap-x-1">
-                    {member.profile.name}
+                    {member?.profile.name}
                     {roleIcon}
                   </div>
                   <p className="text-xs text-zinc-500">
-                    {member.profile.email}
+                    {member?.profile.email}
                   </p>
                 </div>
-                {server.profileId !== member.profileId &&
-                  loadingId !== member.id && (
+                {server.profileId !== member?.profileId &&
+                  loadingId !== member?.id && (
                     <div className="ml-auto">
                       <DropdownMenu>
                         <DropdownMenuTrigger>
@@ -160,23 +160,23 @@ const MembersModel = () => {
                               <DropdownMenuSubContent>
                                 <DropdownMenuItem
                                   onClick={() =>
-                                    onRoleChange(member.id, "GUEST")
+                                    onRoleChange(member?.id, "GUEST")
                                   }
                                 >
                                   <Shield className="h-4 w-4 mr-2" />
                                   Guest
-                                  {member.role === "GUEST" && (
+                                  {member?.role === "GUEST" && (
                                     <Check className="h-4 w-4 ml-auto" />
                                   )}
                                 </DropdownMenuItem>
                                 <DropdownMenuItem
                                   onClick={() =>
-                                    onRoleChange(member.id, "MODERATOR")
+                                    onRoleChange(member?.id, "MODERATOR")
                                   }
                                 >
                                   <ShieldCheck className="h-4 w-4 mr-2" />
                                   Moderator
-                                  {member.role === "MODERATOR" && (
+                                  {member?.role === "MODERATOR" && (
                                     <Check className="h-4 w-4 ml-auto" />
                                   )}
                                 </DropdownMenuItem>
@@ -184,7 +184,7 @@ const MembersModel = () => {
                             </DropdownMenuPortal>
                           </DropdownMenuSub>
                           <DropdownMenuSeparator />
-                          <DropdownMenuItem onClick={() => onKick(member.id)}>
+                          <DropdownMenuItem onClick={() => onKick(member?.id)}>
                             <Gavel className="h-4 w-4 mr-2" />
                             Kickout
                           </DropdownMenuItem>
@@ -193,7 +193,7 @@ const MembersModel = () => {
                     </div>
                   )}
 
-                {loadingId === member.id && (
+                {loadingId === member?.id && (
                   <Loader2 className="animate-spin text-zinc-500 ml-auto h-4 w-4" />
                 )}
               </div>
